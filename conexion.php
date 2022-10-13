5ffda4d28345b3793512e1c3503d8d09a93794f5
EinarOmarVillegasRuiz <98448238+EinarOmarVillegasRuiz@users.noreply.github.com>
2022-10-12 21:30:56 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,16 +1,11 @@
 <?php
-
-    $servidor = "localhost";
-    $user = "root";
-    $psw = "";  
-    $bd = "practica";
-    
-    $conn = mysqli_connect($servidor,$user,$psw,$bd);
-
-    if(mysqli_connect_errno()){
-        //echo'{"response":"0","message":"Error de conexion"}';
-    }else{
-        mysqli_set_charset($conn,"utf8");
-    }
-   
+function cifrar($mensaje,$llave){
+    $inivec = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
+    $ms_encriptado = openssl_encrypt($mensaje,"AES-256-CBC",$llave,0,$inivec);
+    return base64_encode($ms_encriptado."::".$inivec);
+}
+function desifrar($mensaje,$llave){
+    list($datos,$inivec)=explode('::', base64_decode($mensaje),2);
+    return openssl_decrypt($datos,'AES-256-CBC',$llave,0,$inivec);
+}
 ?>
